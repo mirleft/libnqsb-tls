e6c0f3aca9b4f8f019ac04c7114c5fee34fc64b6
Jeremy Yallop <yallop@gmail.com>
2016-06-14 21:09:17 +0100

    Add a licence (MIT) and copyright notices

--- lib/apply_bindings.ml
+++ lib/apply_bindings.ml
@@ -1,1 +1,8 @@
+(*
+ * Copyright (c) 2016 Enguerrand Decorne
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ *)
+
 include Bindings.Stubs(Tls_bindings)
